20ec843d33a949a8421b5a2d8abd5a88d885baa3
SaadKhan665 <saadneo12@gmai.com>
2025-04-15 19:25:21 +0530

    version 3

--- csharp_project/obj/Debug/net7.0/csharp_project.AssemblyInfo.cs
+++ csharp_project/obj/Debug/net7.0/csharp_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca2ea26a1d78d349d0f06bb3bafd737f5433a385")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fa7111b1b5535be62bb254b32a13dbfacd0a3ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
